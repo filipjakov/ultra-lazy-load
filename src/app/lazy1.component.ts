d40bd7382890a03d8c2473c1f9b5d9d03085d9c8
Filip Jakov Bulić <filip.jakov.bulic@infinum.hr>
2020-02-26 21:41:49 +0100

    WIP

---
+++ src/app/lazy1.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  template: `
+    <p>
+      lazy1 works!
+    </p>
+  `,
+  styles: []
+})
+export class Lazy1Component {}
---
+++ src/app/lazy2b.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  template: `
+    <p>
+      lazy2b works!
+    </p>
+  `,
+  styles: []
+})
+export class Lazy2bComponent {}
