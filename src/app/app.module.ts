b5adba5ab421cff631e8eb364525ccbf3affc8b7
Filip Jakov Bulić <filip.jakov.bulic@infinum.hr>
2020-02-26 21:33:36 +0100

    WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { Lazy1Component } from './lazy1.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent, Lazy1Component ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/lazy2.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component, OnInit } from '@angular/core';
-
-@Component({
-  selector: 'app-lazy1',
-  template: '<div>Lazy2</div>',
-})
-export class Lazy2Component {}
